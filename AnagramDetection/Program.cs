a9b1d67a3954d405ade827698ddac7c1e14e80f8
marcustung <marcustung116@gmail.com>
2018-04-06 01:08:25 +0800

    first version

---
+++ AnagramDetection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramDetection
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+	public class Kata
+	{
+		public static bool IsAnagram(string test, string original)
+		{
+			if (string.IsNullOrEmpty(test) || string.IsNullOrEmpty(original))
+				return false;
+
+			var testLookup = GetCollectionLookup(test);
+
+			var originalLookup = GetCollectionLookup(original);
+
+			return LookUpCompare(testLookup, originalLookup);
+		}
+
+		private static ILookup<string, char> GetCollectionLookup(string input)
+		{
+			return input.ToCharArray().ToLookup(p => p.ToString().ToLower());
+		}
+
+		private static bool LookUpCompare(ILookup<string, char> target, ILookup<string, char> source)
+		{
+			foreach (var item in source)
+			{
+				var compare = target.Where(o => o.Key == item.Key && o.Count() == item.Count());
+
+				if (!compare.Any())
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using AnagramDetection;
+
+namespace UnitTestProject1
+{
+	[TestFixture]
+	public class UnitTest1
+	{
+		[TestFixture]
+		public class Tests
+		{
+			[Test]
+			[TestCase("foefet", "toffee", true)]
+			[TestCase("Buckethead", "DeathCubeK", true)]
+			[TestCase("Twoo", "Woot", true)]
+			[TestCase("apple", "pale", false)]
+			public static void FixedTest(string test, string original, bool result)
+			{
+				Assert.AreEqual(result, Kata.IsAnagram(test, original));
+			}
+		}
+	}
+}
